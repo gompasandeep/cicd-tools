a43c98c0fb65e56f2555d7b2a964309b36413ea2
Sandeepgompa <sandeepgompa01@gmail.com>
2025-04-26 18:09:05 +0530

    cicd-tools

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.66.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "82s-remote-state-sandeep"
+    key    = "expense-tools"
+    region = "us-east-1"
+    dynamodb_table = "82s-state-locking"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
